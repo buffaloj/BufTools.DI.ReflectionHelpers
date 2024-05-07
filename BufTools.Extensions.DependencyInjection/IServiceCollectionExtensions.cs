b48136eca6b65f4c63f79820093d7d191c7252dc
Jeremy <shulljeremy@hotmail.com>
2024-05-07 09:48:33 -0400

    Added more multi-assembly methods

--- BufTools.Extensions.DependencyInjection/IServiceCollectionExtensions.cs
+++ BufTools.Extensions.DependencyInjection/IServiceCollectionExtensions.cs
@@ -14,45 +14,96 @@ namespace BufTools.Extensions.DependencyInjection
         /// <summary>
         /// Registers scoped classes for dependency injection that have a baseclass or interface of type T
         /// </summary>
+        /// <returns>The types that were registered</returns>
         /// <typeparam name="T">The type of base class or interface to register classes of</typeparam>
         /// <param name="services">The service collection to register classes with</param>
         /// <param name="assembly">The assembly to search within for class types</param>
-        public static void AddScopedClasses<T>(this IServiceCollection services, Assembly assembly)
+        public static IEnumerable<Type> AddScopedClasses<T>(this IServiceCollection services, Assembly assembly)
         {
             var types = assembly.GetConcreteClasses<T>();
 
             foreach (var type in types)
                 services.AddScoped(type);
+
+            return types;
+        }
+
+        /// <summary>
+        /// Registers scoped classes for dependency injection that have a baseclass or interface of type T
+        /// </summary>
+        /// <returns>The types that were registered</returns>
+        /// <typeparam name="T">The type of base class or interface to register classes of</typeparam>
+        /// <param name="services">The service collection to register classes with</param>
+        /// <param name="assemblies">The assemblies to search within for class types</param>
+        public static IEnumerable<Type> AddScopedClasses<T>(this IServiceCollection services, IEnumerable<Assembly> assemblies)
+        {
+            return assemblies.SelectMany(assembly => services.AddScopedClasses<T>(assembly)).ToList();
         }
 
         /// <summary>
         /// Registers singleton classes for dependency injection that have a baseclass or interface of type T
         /// </summary>
+        /// <returns>The types that were registered</returns>
         /// <typeparam name="T">The type of base class or interface to register classes of</typeparam>
         /// <param name="services">The service collection to register classes with</param>
         /// <param name="assembly">The assembly to search within for class types</param>
-        public static void AddSingletonClasses<T>(this IServiceCollection services, Assembly assembly)
+        public static IEnumerable<Type> AddSingletonClasses<T>(this IServiceCollection services, Assembly assembly)
         {
             var types = assembly.GetConcreteClasses<T>();
 
             foreach (var type in types)
                 services.AddSingleton(type);
+
+            return types;
+        }
+
+        /// <summary>
+        /// Registers singleton classes for dependency injection that have a baseclass or interface of type T
+        /// </summary>
+        /// <returns>The types that were registered</returns>
+        /// <typeparam name="T">The type of base class or interface to register classes of</typeparam>
+        /// <param name="services">The service collection to register classes with</param>
+        /// <param name="assemblies">The assemblies to search within for class types</param>
+        public static IEnumerable<Type> AddSingletonClasses<T>(this IServiceCollection services, IEnumerable<Assembly> assemblies)
+        {
+            return assemblies.SelectMany(assembly => services.AddSingletonClasses<T>(assembly)).ToList();
         }
 
         /// <summary>
         /// Registers transient classes for dependency injection that have a baseclass or interface of type T
         /// </summary>
+        /// <returns>The types that were registered</returns>
         /// <typeparam name="T">The type of base class or interface to register classes of</typeparam>
         /// <param name="services">The service collection to register classes with</param>
         /// <param name="assembly">The assembly to search within for class types</param>
-        public static void AddTransientClasses<T>(this IServiceCollection services, Assembly assembly)
+        public static IEnumerable<Type> AddTransientClasses<T>(this IServiceCollection services, Assembly assembly)
         {
             var types = assembly.GetConcreteClasses<T>();
 
             foreach (var type in types)
                 services.AddTransient(type);
+
+            return types;
         }
 
+        /// <summary>
+        /// Registers transient classes for dependency injection that have a baseclass or interface of type T
+        /// </summary>
+        /// <returns>The types that were registered</returns>
+        /// <typeparam name="T">The type of base class or interface to register classes of</typeparam>
+        /// <param name="services">The service collection to register classes with</param>
+        /// <param name="assemblies">The assemblies to search within for class types</param>
+        public static IEnumerable<Type> AddTransientClasses<T>(this IServiceCollection services, IEnumerable<Assembly> assemblies)
+        {
+            return assemblies.SelectMany(assembly => services.AddTransientClasses<T>(assembly)).ToList();
+        }
+
+        /// <summary>
+        /// Returns a collection of classes that are concrete implementations
+        /// </summary>
+        /// <typeparam name="T">The Type to search for</typeparam>
+        /// <param name="assembly">The assembly to search within</param>
+        /// <returns>An array of types</returns>
         private static Type[] GetConcreteClasses<T>(this Assembly assembly)
         {
             return assembly.GetTypes()
